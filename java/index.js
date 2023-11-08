e434d7e714df3c5af0f1ce27e16504d631574f70
onika <silekuonika02gmail.com>
2023-11-08 12:11:11 +0200

    third commit

--- java/index.js
+++ java/index.js
@@ -15,3 +15,37 @@ let remainder = num1 % num2; // Getting the remainder
 console.log("The remainder when num1 is divided by num2 is: " + remainder);
 
 console.log(`${num1} / ${num2} = ${divisionResult} remainder ${remainder}.`);
+// B. Task 2:Data Types
+// An array of fruits
+let fruits = ['apple', 'banana', 'orange'];
+// Displaying the variables and their types to the console
+console.log(fruits);
+
+console.log(typeof fruits)
+// An object representing information about a person
+
+let person = {
+    firstName: 'Onika',
+    age: 50,
+    city: 'Kuilsriver',
+
+// Checking the type of the 'person' variable
+}console.log(person);
+
+console.log(typeof person); 
+// Checking the type of the 'firstName' property within the 'person' object
+console.log(typeof person.firstName); 
+let colours = [20, 'white','orange', 'green']
+
+console.log(colours[0])
+// 5. Create a null variable and an undefined variable
+let nullVariable = null;
+let undefinedVariable = undefined;
+
+console.log(nullVariable);
+console.log(typeof nullVariable); // Checking the type of the 'nullVariable'
+
+console.log(undefinedVariable);
+console.log(typeof undefinedVariable); // Checking the type of the 'undefinedVariable'
+
+
